023ab7e902631c2c0b61b8da8d46c10db4c687d5
AmanDwivedi2324 <0786ad2000@gmail.com>
2026-08-09 14:54:56 +0530

    MVP done

--- backend/app/api/routes/chat.py
+++ backend/app/api/routes/chat.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
-from utils.youtube_utils import extract_video_id
+
+from app.utils.youtube_utils import extract_video_id
 from app.services.rag_service import answer_question
 from app.services.pinecone_service import video_exists
 from app.services.video_service import ingest_video
@@ -14,18 +15,31 @@ class ChatRequest(BaseModel):
 @router.post("/chat")
 def chat(request: ChatRequest):
 
-    video_id = extract_video_id(request.youtube_url)
+    try:
+        video_id = extract_video_id(request.youtube_url)
 
      # Check whether video is already processed
-    if not video_exists(video_id):
-        ingest_video(request.youtube_url)
+        if not video_exists(video_id):
+            ingest_video(request.youtube_url)
 
 
-    # Now answer using RAG
-    answer = answer_question(question=request.question,video_id=video_id)
+        # Now answer using RAG
+        answer = answer_question(question=request.question,video_id=video_id)
 
-    return {
-        "video_id": video_id,
-        "question": request.question,
-        "answer": answer
-    }
+        return {
+            "video_id": video_id,
+            "question": request.question,
+            "answer": answer
+        }
+    
+    except ValueError as error:
+        raise HTTPException(
+            status_code=400,
+            detail=str(error)
+        )
+    
+    except Exception:
+        raise HTTPException(
+            status_code=500,
+            detail="Something went wrong while processing the request."
+        )
