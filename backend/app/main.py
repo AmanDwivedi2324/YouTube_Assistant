6cc7498cfa6b3ba6733ac2e93d95fec9dd3d383e
AmanDwivedi2324 <0786ad2000@gmail.com>
2026-08-09 15:54:37 +0530

    MVP Done

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes.chat import router as chat_router
 from app.api.routes.video import router as video_router
 
 app = FastAPI(
     title="YouTube Assistant API",
     description="RAG-based YouTube chatbot"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(chat_router)
